42ec1b7ff16db8d0d6f269fedc1f8f4cd74453d2
kjj13 <rlawndwo123@hanmail.net>
2018-11-13 06:17:14 +0900

    temporary trans.c

--- trans.c
+++ trans.c
@@ -3,19 +3,32 @@
 int instr_trans(char *op, char *args, char* mcode)
 {
 	// check syntax 
+	char *poppy;
+	char *des;
+	char str[128];
+	
+	strcpy(str,args);
+	poppy = strtok(str,",");
+	des = strtok(NULL , "\0");
+
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
 	}
 
-
-	strcpy(mcode, "AB CD EF");
-
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
+	if(poppy[0] == '%' && des[0] == '%')
+		strcpy(mcode , "89");
+	else if((poppy[0] == '(' && des[0] =='%') || (poppy[0] == '-' && des[0] == '%'))
+		strcpy(mcode , "8b");
+	else if(poppy[0] == '0' && strstr(des,"%eax") != NULL)
+		strcpy(mcode , "a1");
+	else if(strstr(poppy,"%eax") != NULL && des[0] == '0')
+		strcpy(mcode , "a3");
+	else if(poppy[0] == '$' && des[0] == '%')
+	{
+		strcpy(mcode , "b8");
+	}
+		
 	
 	return 1;	
 }
